6dd14bcef2812197674ae8845d57c64baeec116d
Viet Hoang <ngviethoang0212@gmail.com>
2018-04-19 21:48:58 +0700

    update history

--- public/.lib/js/script.js
+++ public/.lib/js/script.js
@@ -1,9 +1,6 @@
 $(document).ready(() => {
     const MOVIES_BASE_DIR = 'Movies/'
 
-    // const BASE_URL = 'http://192.168.0.109:6969' //Cable
-    const BASE_URL = 'http://192.168.0.118:6969' //WiFi
-
     let player = videojs('myVideo')
     player.seekButtons({
         forward: 10,
@@ -22,13 +19,15 @@ $(document).ready(() => {
     })
 
     new Vue({
-        el: '#listContainer',
+        el: '#vue',
         data: {
             movies: [],
+            tmpMovies: [], //store all movies while showing watched movies
             seriesList: [],
+            history: null
         },
         created() {
-            $.getJSON(BASE_URL + '/api/movies', data => {
+            $.getJSON('api/movies', data => {
                 for(let file of data) {
                     switch (file.type) {
                         case 'movies':
@@ -38,6 +37,13 @@ $(document).ready(() => {
                             this.seriesList.push(file)
                             break
                     }
+                }
+                this.history = JSON.parse(localStorage.getItem('history'))
+                if(this.history !== null) {
+                    this.movies = this.movies.map(m => {
+                        m.watched = this.history.movies.includes(m.dir)
+                        return m
+                    })
                 }
             })
         },
@@ -50,6 +56,21 @@ $(document).ready(() => {
                 let subtitlePath = (sub !== undefined) ? (MOVIES_BASE_DIR + movie.dir + '/' + sub.name) : undefined
 
                 this.setPlayer(movie.name, videoPath, subtitlePath)
+
+                //Save to history
+                this.history = JSON.parse(localStorage.getItem('history'))
+                if(this.history === null) {
+                    this.history = {movies: [], series: []}
+                }
+                let mIndex = this.history.movies.findIndex(m => m === movie.dir)
+                if(mIndex !== -1) {
+                    this.history.movies.splice(mIndex, 1)
+                }
+                this.history.movies.unshift(movie.dir)
+                localStorage.setItem('history', JSON.stringify(this.history))
+
+                movie.watched = true
+                this.$nextTick()
             },
             showMovieInSeries(series, file) {
                 let videoPath = (file.name !== undefined) ? (MOVIES_BASE_DIR + series.dir + '/' + file.name) : undefined
@@ -78,6 +99,41 @@ $(document).ready(() => {
 
                     $('.videoInfo').find('h2 > span').html(videoName)
                 }
+            },
+            showLatestMovie() {
+                if(this.history.movies.length > 0) {
+                    let movie = this.movies.find(m => m.dir === this.history.movies[0])
+                    if(movie !== undefined) {
+                        this.showMovie(movie)
+                    }
+                }
+            },
+            toggleWatchedMovies(e) {
+                let btnStatus
+                if(this.tmpMovies.length > 0) { //Show all movies
+                    this.movies = this.tmpMovies
+                    this.tmpMovies = []
+
+                    btnStatus = 'Watched'
+                } else {
+                    this.tmpMovies = this.movies
+                    this.movies = this.movies.filter(m => m.watched)
+
+                    btnStatus = 'All'
+                }
+                if(e !== undefined)
+                    $(e.target).find('span').html(btnStatus)
+            },
+            clearHistory() {
+                if(this.tmpMovies.length > 0) {
+                    this.movies = this.tmpMovies
+                    this.tmpMovies = []
+                }
+                this.history = null
+                localStorage.removeItem('history')
+
+                this.movies.forEach(m => m.watched = false)
+                this.$nextTick()
             }
         }
     })
